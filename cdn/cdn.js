3b7d6a55a0377caf84c1c8a74267c024edb9863c
dnitjm <comma@tuta.io>
2016-05-09 21:30:48 +0800

    cdn

--- cdn/cdn.js
+++ cdn/cdn.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$('#form').appendTo('.map_bg');
     $('.btn-toolbar').appendTo('.map_bg');
     $('.row').prependTo('.map_bg');
+    setTimeout(function() {
+    	$('.container').fadeIn('slow');
+    }, 1500)
 });
