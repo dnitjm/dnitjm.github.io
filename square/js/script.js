5be72e2fd3bb5b5933561ede7f4136e7dbdeeed8
lfgchannel <lfgch@git-eml.com>
2016-11-21 21:26:59 +0300

    img paths fixed

--- square/js/script.js
+++ square/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
   $('#myparallax').prllxx({
-    base : '../img/',
+    base : 'https://dnitjm.github.io/square/img/',
     name : 'scroll-',
     ext  : '.jpg',
     total: 20
   });
   $('#mysecond').prllxx({
-    base : '../img/',
+    base : 'https://dnitjm.github.io/square/img/',
     name : 'scroll',
     ext  : '.png',
     total: 7
